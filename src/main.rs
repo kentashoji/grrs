de9643bf421b710083e3d5c0f2048f3e785869bd
kentashoji <kenta_shoji@azoop.co.jp>
2022-08-17 00:34:28 +0900

    https://rust-cli.github.io/book/tutorial/testing.html#making-your-code-testable

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,14 @@ fn main() {
     let file = std::fs::File::open(&args.path).expect("could not read file");
     let content = BufReader::new(file);
 
+    find_matches(content, &args.pattern);
+}
+
+fn find_matches<R: BufRead>(content: R, pattern: &str) {
     for line in content.lines() {
         let l = line.expect("Unable to read line");
-        if l.contains(&args.pattern) {
-            println!("{}", l)
+        if l.contains(pattern) {
+            println!("{}", l);
         }
     }
 }
